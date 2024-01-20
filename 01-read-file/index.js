59f64295f7dc6d30c4004129aaed2f4792293f17
Gleb <71218071+b1ood@users.noreply.github.com>
2024-01-21 00:42:21 +0300

    feat: add solution to 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,15 +2,6 @@ const path = require('node:path');
 const fs = require('node:fs');
 const textPath = path.join(__dirname, 'text.txt');
 
-const readFileAsync = async (path) => {
-  return new Promise((resolve, reject) =>
-    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
-      if (err) {
-        return reject(err.message);
-      }
-      resolve(data);
-    }),
-  );
-};
+const readableStream = fs.createReadStream(textPath, { encoding: 'utf-8' });
 
-readFileAsync(textPath).then((data) => console.log(data));
+readableStream.on('data', (chunk) => process.stdout.write(chunk));
