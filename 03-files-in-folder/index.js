fdeef2e5b909661e18ec1b11225ba5b9e8662cd3
Gleb <71218071+b1ood@users.noreply.github.com>
2024-01-23 20:31:24 +0300

    feat: correct readFileParams function

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,9 +5,7 @@ const dirPath = path.resolve(__dirname, 'secret-folder');
 function readFileParams(fileName, fileExt, filePath) {
   fs.stat(filePath, (err, stats) => {
     if (err) console.log(err.message);
-    console.log(
-      `${fileName} - ${!fileExt ? 'N/A' : fileExt} - ${stats.size / 1000}kB`,
-    );
+    console.log(`${fileName} - ${fileExt} - ${stats.size / 1000}kB`);
   });
 }
 
@@ -15,9 +13,6 @@ function reader(folderPath) {
   fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
     if (err) console.log(err);
     files.forEach((file) => {
-      if (file.isDirectory()) {
-        return reader(path.resolve(dirPath, file.name));
-      }
       if (file.isFile()) {
         const filePath = path.join(folderPath, file.name);
         const fileExt = path.extname(filePath).slice(1);
